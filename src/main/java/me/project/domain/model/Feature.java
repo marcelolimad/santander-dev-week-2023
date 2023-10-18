9f17a53653f1974b6cfea1c74c20f295f09779c3
marcelolimad <marcelolimad@gmail.com>
2023-10-17 21:09:24 -0300

    Implementação da camada de dominio

---
+++ src/main/java/me/project/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package me.project.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/project/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.project.domain.repository;
+
+import me.project.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
